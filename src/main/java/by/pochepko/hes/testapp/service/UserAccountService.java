cd5d74b4525a354c5ad346ca1d748255bce3d94a
pochepko_oleg <pochepkooleg@gmail.com>
2020-02-17 15:26:20 +0300

    fixes

--- src/main/java/by/pochepko/hes/testapp/service/UserAccountService.java
+++ src/main/java/by/pochepko/hes/testapp/service/UserAccountService.java
@@ -13,27 +13,47 @@ import java.util.List;
 public interface UserAccountService {
 
     /**
+     * Return created user entity
+     *
      * @param userAccount must not be null
      * @return created entity
      */
     UserAccountDto createUser(UserAccountDto userAccount);
 
     /**
-     * @param id must not be null
+     * Return user entity with given id
+     *
+     * @param id
      * @return the entity with the given id
      */
 
     UserAccountDto getUserAccountById(long id);
 
     /**
+     * Update user detail for existing user identified by passed id
+     *
      * @param userAccount must not be null
      * @param id          must not be null
      */
 
     void updateUserAccount(UserAccountDto userAccount, long id);
 
+    /**
+     * Return list of user entity on page depending on its size
+     *
+     * @param page
+     * @param size
+     * @return List of users
+     */
+
 
     List<UserAccountDto> getUserAccountsList(int page, int size);
+
+    /**
+     * Return total count of entities in repository
+     *
+     * @return total count
+     */
 
 
     long getTotalCount();
